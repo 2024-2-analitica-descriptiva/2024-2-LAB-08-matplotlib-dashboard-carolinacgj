5fde7d43dc393c3475a2c8049b254500e073a199
carolinacgj <cgarcesj@unal.edu.co>
2025-01-10 19:01:34 -0500

    intento1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -3,8 +3,11 @@
 Escriba el codigo que ejecute la accion solicitada.
 """
 
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
 
-def pregunta_01():
+def pregunta():
     """
     El archivo `files//shipping-data.csv` contiene información sobre los envios
     de productos de una empresa. Cree un dashboard estático en HTML que
@@ -35,3 +38,136 @@ def pregunta_01():
     * Su código debe crear la carpeta `docs` si no existe.
 
     """
+def crear_carpeta():
+        # Crear las carpetas si no existen
+    output_dir = "docs"
+    try:
+        os.makedirs(output_dir, exist_ok=True)
+    except Exception as e:
+        print(f"Error al crear el directorio {output_dir}: {e}")
+        return
+
+#cargar datos
+def load_data():
+    df=pd.read_csv("files/input/shipping-data.csv")
+    return df
+
+def create_visual_for_shipping_per_warehouse(df):
+    df=df.copy()
+    plt.figure()
+    counts=df.Warehouse_block.value_counts()
+    counts.plot.bar(
+        title='Shipping per Warehouse',
+        xlabel='Warehouse block',
+        ylabel='Record Count',
+        color='tab:blue',
+        fontsize=8,
+    )
+
+    plt.gca().spines['top'].set_visible(False)
+    plt.gca().spines['right'].set_visible(False)
+    plt.savefig("docs/shipping_per_warehouse.png")
+
+
+def create_visual_for_mode_of_shipment(df):
+    df=df.copy()
+    plt.figure()
+    counts=df.Mode_of_Shipment.value_counts()
+    counts.plot.pie(
+        title='Mode of Shipment',
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=['tab:blue','tab:orange','tab:green'],
+    )
+
+    plt.savefig("docs/mode_of_shipment.png")
+
+
+def create_visual_for_average_customer_rating(df):
+    df=df.copy()
+    plt.figure()
+    df=(
+        df[["Mode_of_Shipment", "Customer_rating"]]
+        .groupby("Mode_of_Shipment")
+        .describe()
+    )
+
+    df.columns=df.columns.droplevel()
+    df=df[["mean","min","max"]]
+    plt.barh(
+        y=df.index.values,
+        width=df["max"].values-1,
+        left=df["min"].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.8,
+    )
+
+    colors=[
+        "tab:green" if value >= 3.0 else "tab:orange" for value in df["mean"].values
+    ]
+
+    plt.barh(
+        y=df.index.values,
+        width=df["mean"].values-1,
+        left=df["min"].values,
+        height=0.5,
+        color=colors,
+        alpha=1.0,
+    )
+
+    plt.title("Average Customer Rating")
+    plt.gca().spines["left"].set_color("gray")
+    plt.gca().spines["bottom"].set_color("gray")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    plt.savefig("docs/average_customer_rating.png")
+
+    return colors
+
+def create_visual_for_weight_distribution(df):
+    df=df.copy()
+    plt.figure()
+    df.Weight_in_gms.plot.hist(
+        title="Shipped Weight Distribution",
+        color="tab:orange",
+        edgecolor="white"
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    plt.savefig("docs/weight_distribution.png")
+
+def create_html():
+        with open("docs/index.html", "w") as file:
+            file.write("""
+        <!DOCTYPE html>
+        <html>
+            <body>
+                <h1>Shipping Dashboard Example</h1>
+                <div style="width:45%;float:left">
+                    <img src="shipping_per_warehouse.png" alt="Fig 1">
+                    <img src="mode_of_shipment.png" alt="Fig 2">
+                </div>
+                <div style="width:45%;float:left">
+                    <img src="average_customer_rating.png" alt="Fig 3">
+                    <img src="weight_distribution.png" alt="Fig 4">
+                </div>
+            <body>
+        </html>
+            """)
+
+
+def pregunta_01():
+    crear_carpeta()
+    df=load_data()
+    create_visual_for_shipping_per_warehouse(df)
+    create_visual_for_mode_of_shipment(df)
+    create_visual_for_average_customer_rating(df)
+    create_visual_for_weight_distribution(df)
+    create_html()
+
+
+if __name__ == "__main__":
+    pregunta_01()
